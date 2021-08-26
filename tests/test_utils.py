141bbbb14087ee12c21e6a6563732a0eb892686c
CarolineMorton <carolinemorton@gmx.com>
2021-08-26 12:55:34 +0100

    remove unneeded test file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ from cohortreport.utils import load_config
 
 
 class TestLoadConfig:
-    def test_updated_correctly(self):
+    def test_all_updated(self):
         test_config = {"output_path": "test_path", "variable_types": "test_typing"}
 
         observed_config = load_config(test_config)
@@ -15,7 +15,7 @@ class TestLoadConfig:
         observed_config = load_config(test_config)
 
         assert observed_config["output_path"] == "test_path"
-        assert observed_config["variable_types"] == None
+        assert observed_config["variable_types"] is None
 
     def test_defaults_only(self):
         test_config = {}
@@ -23,4 +23,4 @@ class TestLoadConfig:
         observed_config = load_config(test_config)
 
         assert observed_config["output_path"] == "cohort_reports_outputs/"
-        assert observed_config["variable_types"] == None
+        assert observed_config["variable_types"] is None
