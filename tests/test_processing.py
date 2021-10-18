7d59fb83434c86a5fad62a337cb4b6a539ddbdf9
CarolineMorton <carolinemorton@gmx.com>
2021-10-18 16:19:15 +0100

    blacken code

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -121,10 +121,14 @@ class TestTypeVariables:
 
 class TestBinaryToCategorical:
     def test_change_binary_to_categorical(self):
-        assert is_categorical_dtype(processing.change_binary_to_categorical(pd.Series([0, 1])))
+        assert is_categorical_dtype(
+            processing.change_binary_to_categorical(pd.Series([0, 1]))
+        )
 
     def test_does_not_change_floats_to_categorical(self):
-        assert is_float_dtype(processing.change_binary_to_categorical(pd.Series([0.0, 1.0])))
+        assert is_float_dtype(
+            processing.change_binary_to_categorical(pd.Series([0.0, 1.0]))
+        )
 
     def test_does_not_change_numeric_to_categorical(self):
         exp = pd.Series([1, 3])
