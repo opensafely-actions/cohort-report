ac1805480b446a145e8cc99cf18ef6909e60ca7f
Iain Dillingham <iain@dillingham.me.uk>
2022-01-10 12:10:25 +0000

    Join `suffixes` for more readable error message

--- cohortreport/report.py
+++ cohortreport/report.py
@@ -45,8 +45,8 @@ def make_report(
         )
 
     # validate that config passed matches with file type
-    ext = path.suffixes
-    if (ext == [".csv"] or ext == [".csv", ".gz"]) and variable_types is None:
+    ext = "".join(path.suffixes)
+    if (ext == ".csv" or ext == ".csv.gz") and variable_types is None:
         raise ConfigAndFileMismatchError(
             f"You have loaded a file type - {ext} that expects "
             f"a variables_types config to be passed in."
